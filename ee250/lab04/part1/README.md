289f87d1cb143aed289efa8e3625ba2f36f3ada2
Jason A. Tran <jasontra@usc.edu>
2018-02-05 14:43:08 -0800

    ee250/lab04/part1: init commit

---
+++ ee250/lab04/part1/README.md
@@ -0,0 +1,8 @@
+## **EE 250L Spring 2018 Lab 04 Part 1**
+
+The code in this directory is from just the TCP portion of the YouTube tutorial 
+linked below. First, watch the video. Then, read through the tcpClient.py and 
+tcpServer.py files. Run the files like in the video to test out the program. 
+Once you are done, move on to part 2.
+
+https://www.youtube.com/watch?v=bTThyxVy7Sk&index=6&list=PL1A2CSdiySGIPxpSlgzsZiWDavYTAx61d
---
+++ ee250/lab04/part1/tcpClient.py
@@ -0,0 +1,38 @@
+"""Example python3 client-side socket program using TCP from the tutorial video 
+below.
+
+https://www.youtube.com/watch?v=bTThyxVy7Sk&index=6&list=PL1A2CSdiySGIPxpSlgzsZiWDavYTAx61d
+"""
+
+"""python3 in Ubuntu (and other linux distros) will look in a default set of
+directories for libraries when a program tries to import one. Examples of some
+default directories are:
+  /usr/lib/python3.5
+  /usr/local/lib/python3.5/dist-packages
+"""
+import socket
+
+def Main():
+    """127.0.0.1 is the loopback address. Any packets sent to this address will
+    essentially loop right back to your machine and look for any process 
+    listening in on the port specified."""
+    host = '127.0.0.1'
+    port = 5000
+
+    s = socket.socket()
+    s.connect((host,port))
+
+    message = input("-> ")
+    while message != 'q':
+        s.send(message.encode('utf-8')) 
+        #1024 is the receive buffer size. It's enough for us, and it's a nice number. 
+        data = s.recv(1024).decode('utf-8') 
+        print("Received from server: " + data)
+        message = input("-> ")
+    s.close()
+
+"""This if-statement checks if you are running this python file directly. That 
+is, if you run `python3 tcpClient.py` in terminal, this if-statement will be 
+true"""
+if __name__ == '__main__':
+    Main()
