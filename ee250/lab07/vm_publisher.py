5fb228cb94075831f0bd9df9b82389dc12afc8f5
Jason A. Tran <jasonatran@gmail.com>
2018-03-06 15:17:46 -0800

    lab07: init commit of skeleton code

---
+++ ee250/lab07/vm_publisher.py
@@ -0,0 +1,55 @@
+"""EE 250L Lab 07 Skeleton Code
+
+Run vm_publisher.py in a separate terminal on your VM."""
+
+import paho.mqtt.client as mqtt
+import time
+from pynput import keyboard
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code "+str(rc))
+
+    #subscribe to topics of interest here
+
+#Default message callback. Please use custom callbacks.
+def on_message(client, userdata, msg):
+    print("on_message: " + msg.topic + " " + str(msg.payload))
+
+def on_press(key):
+    try: 
+        k = key.char # single-char keys
+    except: 
+        k = key.name # other keys
+    
+    if k == 'w':
+        print("w")
+        #send "w" character to rpi
+    elif k == 'a':
+        print("a")
+        # send "a" character to rpi
+        #send "LED_ON"
+    elif k == 's':
+        print("s")
+        # send "s" character to rpi
+    elif k == 'd':
+        print("d")
+        # send "d" character to rpi
+        # send "LED_OFF"
+
+if __name__ == '__main__':
+    #setup the keyboard event listener
+    lis = keyboard.Listener(on_press=on_press)
+    lis.start() # start to listen on a separate thread
+
+    #this section is covered in publisher_and_subscriber_example.py
+    client = mqtt.Client()
+    client.on_message = on_message
+    client.on_connect = on_connect
+    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+    client.loop_start()
+
+    while True:
+        print("delete this line")
+        time.sleep(1)
+            
+
---
+++ ee250/lab07/vm_subscriber.py
@@ -0,0 +1,31 @@
+"""EE 250L Lab 07 Skeleton Code
+
+Run vm_subscriber.py in a separate terminal on your VM."""
+
+import paho.mqtt.client as mqtt
+import time
+from pynput import keyboard
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code "+str(rc))
+
+    #subscribe to the ultrasonic ranger topic here
+
+#Default message callback. Please use custom callbacks.
+def on_message(client, userdata, msg):
+    print("on_message: " + msg.topic + " " + str(msg.payload))
+
+def on_press(key):
+if __name__ == '__main__':
+    #this section is covered in publisher_and_subscriber_example.py
+    client = mqtt.Client()
+    client.on_message = on_message
+    client.on_connect = on_connect
+    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+    client.loop_start()
+
+    while True:
+        print("delete this line")
+        time.sleep(1)
+            
+
