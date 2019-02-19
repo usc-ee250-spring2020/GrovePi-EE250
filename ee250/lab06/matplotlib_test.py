bdd44b898e77815460d94b39f1592fa8acc6b5f7
Kwame Wright <kwamelaw@usc.edu>
2019-02-19 09:18:37 -0800

    Comments

--- ee250/lab06/matplotlib_test.py
+++ ee250/lab06/matplotlib_test.py
@@ -1,5 +1,12 @@
 import matplotlib.pyplot as plt
 
+# The following plot line is equivalent to:
+#    plt.plot([0, 1, 2, 3], [1, 2, 3, 4])
+# where [0, 1, 2, 3] represents x values
+# and [1, 2, 3, 4] represents y values
+
 plt.plot([1, 2, 3, 4])
+#plt.plot([0, 1, 2, 3], [1, 2, 3, 4])
+
 plt.ylabel('some numbers')
 plt.show()
