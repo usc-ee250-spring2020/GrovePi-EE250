04ac50c620f734f0437035e0820999e4be048af7
Jason A. Tran <jasonatran@gmail.com>
2019-09-09 23:17:10 -0700

    lab03: add comments indicating more imports may be needed

--- ee250/lab03/part3/ledServer.py
+++ ee250/lab03/part3/ledServer.py
@@ -5,7 +5,8 @@
 
 import sys
 # By appending the folder of all the GrovePi libraries to the system path here,
-# we are successfully `import grovepi`
+# we are able to successfully `import grovepi` below. You may need more for your
+# assignment.
 sys.path.append('../../../Software/Python/')
 
 import grovepi
--- ee250/lab03/part3/ultrasonicClient.py
+++ ee250/lab03/part3/ultrasonicClient.py
@@ -6,7 +6,8 @@
 
 import sys
 # By appending the folder of all the GrovePi libraries to the system path here,
-# we are able to successfully `import grovepi`
+# we are able to successfully `import grovepi` below. You may need more for your
+# assignment.
 sys.path.append('../../../Software/Python/')
 
 import grovepi
